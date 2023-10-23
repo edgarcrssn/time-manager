518607bb075c8ef70fa27b5679d33738ed6f4ea8
Edgar Cresson <76485280+edgarcrssn@users.noreply.github.com>
2023-10-23 16:58:07 +0200

    added users schema

---
+++ priv/repo/migrations/20231023144526_create_users.exs
@@ -0,0 +1,12 @@
+defmodule TimeManagerApi.Repo.Migrations.CreateUsers do
+  use Ecto.Migration
+
+  def change do
+    create table(:users) do
+      add :username, :string, null: false
+      add :email, :string, null: false
+    end
+
+    execute("ALTER TABLE users ADD CONSTRAINT email_format_check CHECK (email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}$')")
+  end
+end
