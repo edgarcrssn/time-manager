fb3ab4fe8f2639662646fd6ac9239f7585657d93
GVercaemer <geoffrey.vercaemer@gmail.com>
2023-10-24 15:47:02 +0200

    Fix HTTP Errors Handling to ClockController
    TM-25

--- lib/time_manager_api_web/controllers/clock_controller.ex
+++ lib/time_manager_api_web/controllers/clock_controller.ex
@@ -8,13 +8,23 @@ defmodule TimeManagerApiWeb.ClockController do
     try do
       clocks = TimeManagerApi.Repo.all(from c in TimeManagerApi.Clock, where: c.user_id == ^user_id)
       case clocks do
-        [] -> send_resp(conn, 404, "Clock not found")
-        _ -> json(conn, clocks)
+        [] ->
+          conn
+          |> put_status(:not_found)
+          |> json(%{"message" => "Clock not found"})
+        _ ->
+          conn
+          |> put_status(:ok)
+          |> json(clocks)
       end
     rescue
-      _ -> send_resp(conn, 500, "Internal Server Error")
+      _ ->
+        conn
+        |> put_status(:internal_server_error)
+        |> json(%{"message" => "Internal Server Error"})
     end
   end
+
 
   def create(conn, %{"userID" => user_id, "clock" => clock_params}) when is_binary(user_id) do
     user_id = String.to_integer(user_id)
@@ -25,6 +35,7 @@ defmodule TimeManagerApiWeb.ClockController do
         {:ok, clock} ->
           conn
           |> put_status(:created)
+          |> json(%{"message" => "Clock correctly created"})
           |> json(clock)
         {:error, changeset} ->
           conn
@@ -32,7 +43,10 @@ defmodule TimeManagerApiWeb.ClockController do
           |> json(%{errors: Ecto.Changeset.traverse_errors(changeset, fn {msg, opts} -> msg end)})
       end
     rescue
-      _ -> send_resp(conn, 500, "Internal Server Error")
+      _ ->
+        conn
+        |> put_status(:internal_server_error)
+        |> json(%{"message" => "Internal Server Error"})
     end
   end
 end
