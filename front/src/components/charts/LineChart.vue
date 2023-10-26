315390a89f5120df4fb8dec5251b910af14800b9
Edgar Cresson <76485280+edgarcrssn@users.noreply.github.com>
2023-10-26 15:48:13 +0200

    msg

--- front/src/components/charts/LineChart.vue
+++ front/src/components/charts/LineChart.vue
@@ -1,16 +1,16 @@
 <template>
-  <LineChart :data="chartData" :options="chartOptions" />
+  <Line :data="chartData" :options="chartOptions" />
 </template>
 
 <script lang="ts">
-import { Line as LineChart } from 'vue-chartjs'
+import { Line } from 'vue-chartjs'
 import { Chart as ChartJS, Title, Tooltip, Legend, PointElement, LineElement } from 'chart.js'
 
 ChartJS.register(Title, Tooltip, Legend, PointElement, LineElement)
 
 export default {
   name: 'LineChart',
-  components: { LineChart },
+  components: { Line },
   props: {
     chartData: {
       type: Object,
--- front/src/pages/working-times.vue
+++ front/src/pages/working-times.vue
@@ -1,12 +1,11 @@
 <script lang="ts">
 import { ref, onMounted, onBeforeMount } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
-import WorkingTime from '../components/WorkingTime.vue'
 import BarChart from '../components/charts/BarChart.vue'
 
 export default {
-  name: 'WorkingTimePage',
-  components: { WorkingTime, BarChart },
+  name: 'WorkingTimesPage',
+  components: { BarChart },
   setup() {
     const route = useRoute()
     const router = useRouter()
@@ -66,42 +65,10 @@ export default {
 </script>
 
 <template>
-  <div class="p-2 flex flex-col items-center">
+  <section class="p-2 flex flex-col items-center">
     <h1 class="text-3xl font-bold underline">Working time</h1>
-    <ul class="w-full max-w-md">
-      <li v-for="workingTime in workingTimes" :key="workingTime.id">
-        <BarChart
-          :chart-data="{
-            labels: ['j', 'f', 'm', 'a', 'm', 'j', 'j'],
-            datasets: [
-              {
-                label: 'My First Dataset',
-                data: [65, 59, 80, 81, 56, 55, 40],
-                backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(255, 159, 64, 0.2)',
-                  'rgba(255, 205, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(201, 203, 207, 0.2)',
-                ],
-                borderColor: [
-                  'rgb(255, 99, 132)',
-                  'rgb(255, 159, 64)',
-                  'rgb(255, 205, 86)',
-                  'rgb(75, 192, 192)',
-                  'rgb(54, 162, 235)',
-                  'rgb(153, 102, 255)',
-                  'rgb(201, 203, 207)',
-                ],
-                borderWidth: 1,
-              },
-            ],
-          }"
-        />
-        <WorkingTime :working-time="workingTime" />
-      </li>
-    </ul>
-  </div>
+    <article class="w-full max-w-md">
+      {{ workingTimes }}
+    </article>
+  </section>
 </template>
