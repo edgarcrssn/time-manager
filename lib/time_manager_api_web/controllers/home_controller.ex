54b1f6a27976da013382cc908cb2ab37bd0e3a4e
Edgar Cresson <76485280+edgarcrssn@users.noreply.github.com>
2023-10-26 10:04:29 +0200

    removed s

--- lib/time_manager_api_web/controllers/home_controller.ex
+++ lib/time_manager_api_web/controllers/home_controller.ex
@@ -6,7 +6,7 @@ defmodule TimeManagerApiWeb.HomeController do
     <!DOCTYPE html>
     <html lang="en">
     <head>
-        <meta charset="UTF-8">s
+        <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>TimeManager API</title>
         <style>
