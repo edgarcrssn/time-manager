1da4dced68e8e9a06174e111cde86127befe6061
mlavalle <marc-aurele.lavalle@epitech.eu>
2023-10-29 10:47:10 +0100

    barchart by week

--- front/src/components/ChartManager.vue
+++ front/src/components/ChartManager.vue
@@ -1,47 +1,107 @@
 <template>
-    <section>
-        <h2 class="text-3xl mt-4 mb-4">User {{ userId }}'s Charts</h2>
+  <section>
+    <h2 class="text-3xl mt-4 mb-4">
+      User {{ userId }}'s Charts
+    </h2>
 
-        <div class="grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
-            <div>
-                <BarChart :data="barChartData" :options="chartOptions" />
-            </div>
-            <div>
-                <PieChart :data="pieChartData" :options="chartOptions" />
-            </div>
-            <div>
-                <LineChart :data="lineChartData" :options="chartOptions" />
-            </div>
-        </div>
-    </section>
+    <button @click="previousWeek">
+      Previous Week
+    </button>
+    <button @click="nextWeek">
+      Next Week
+    </button>
+
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4 p-4">
+      <div>
+        <BarChart
+          :data="barChartData"
+          :options="chartOptions"
+        />
+      </div>
+    </div>
+  </section>
 </template>
-  
-<script lang="ts" setup>
-import { ref } from 'vue';
-import BarChart from './charts/BarChart.vue';
-import PieChart from './charts/PieChart.vue';
-import LineChart from './charts/LineChart.vue';
-import { defineProps } from 'vue';
 
-const { userId } = defineProps(['userId']);
+<script lang="ts" setup>
+import { ref, onMounted, defineProps } from 'vue'
+import BarChart from './charts/BarChart.vue'
+
+const { userId } = defineProps(['userId'])
 
 const barChartData = ref({
-    labels: ['January', 'February', 'March'],
-    datasets: [{ data: [40, 20, 12] }]
-});
-
-const pieChartData = ref({
-    labels: ['Red', 'Blue', 'Yellow'],
-    datasets: [{ data: [300, 50, 100] }]
-});
-
-const lineChartData = ref({
-    labels: ['January', 'February', 'March'],
-    datasets: [{ data: [65, 59, 80] }]
-});
+  labels: [],
+  datasets: [{ data: [], label: 'Hours Worked', backgroundColor: 'blue' }]
+})
 
 const chartOptions = ref({
-    responsive: true,
-    maintainAspectRatio: false
-});
+  responsive: true,
+  maintainAspectRatio: false
+})
+
+const API_URL = `${import.meta.env.VITE_API_URL}/api/workingtimes/${userId}`
+
+const currentDate = new Date()
+
+const getWeekRange = (date) => {
+  const monday =
+    date.getDay() === 0
+      ? new Date(date.setDate(date.getDate() - 6))
+      : new Date(date.setDate(date.getDate() - date.getDay() + 1))
+  const sunday = new Date(date.setDate(date.getDate() + 6))
+
+  return { start: monday, end: sunday }
+}
+
+const fetchWeekData = async (date) => {
+  const { start, end } = getWeekRange(date)
+
+  const response = await fetch(API_URL)
+  const data = await response.json()
+
+  const weekData = data.filter((item) => {
+    const startDate = new Date(item.start).toISOString().split('T')[0]
+    return startDate >= start.toISOString().split('T')[0] && startDate <= end.toISOString().split('T')[0]
+  })
+
+  const aggregatedHours = {}
+
+  weekData.forEach((item) => {
+    const start = new Date(item.start)
+    const end = new Date(item.end)
+    const hours = (end - start) / (1000 * 60 * 60)
+    const date = start.toISOString().split('T')[0]
+
+    if (!aggregatedHours[date]) {
+      aggregatedHours[date] = 0
+    }
+    aggregatedHours[date] += hours
+  })
+
+  const labels = Array.from({ length: 7 }).map((_, i) => {
+    const date = new Date(start)
+    date.setDate(date.getDate() + i)
+    return date.toISOString().split('T')[0]
+  })
+
+  const hoursWorked = labels.map((label) => aggregatedHours[label] || 0)
+
+  barChartData.value = {
+    labels: labels,
+    datasets: [{ data: hoursWorked, label: 'Hours Worked', backgroundColor: 'blue' }]
+  }
+}
+
+const previousWeek = () => {
+  currentDate.setDate(currentDate.getDate() - 7)
+  fetchWeekData(currentDate)
+}
+
+const nextWeek = () => {
+  currentDate.setDate(currentDate.getDate() + 7)
+  fetchWeekData(currentDate)
+}
+
+onMounted(() => {
+  fetchWeekData(currentDate)
+})
 </script>
