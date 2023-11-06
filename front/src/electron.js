8ed369f24dc2f947f6adce3dfa5fdb560cd81ed3
mlavalle <marc-aurele.lavalle@epitech.eu>
2023-11-06 11:01:28 +0100

    add electron.js

---
+++ front/src/electron.js
@@ -0,0 +1,37 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+function createWindow() {
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  win.loadURL(
+    isDev
+      ? 'http://localhost:5173'
+      : `file://${path.join(__dirname, '../dist/index.html')}`
+  );
+
+  if (isDev) {
+    win.webContents.openDevTools();
+  }
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
