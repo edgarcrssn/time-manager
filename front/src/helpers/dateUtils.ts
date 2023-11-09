ab7ad0f366a8f1d98f215ca3e281716809b0f5a8
Ugo Bertrand <ugo.bertrand@epitech>
2023-11-09 12:28:40 +0100

    create the notification to prevent the user to clock in/out

--- front/src/helpers/dateUtils.ts
+++ front/src/helpers/dateUtils.ts
@@ -1,10 +1,16 @@
 export const formatDate = (dateString: string): string => {
   return new Date(dateString).toLocaleDateString('en-US', {
+   
     year: 'numeric',
+   
     month: 'long',
+   
     day: 'numeric',
+   
     hour: '2-digit',
+   
     minute: '2-digit'
+ 
   })
 }
 
@@ -19,4 +25,17 @@ export const getTimeElapsedSinceDate = (dateString: string): string => {
   const seconds = Math.floor((timeElapsed % (1000 * 60)) / 1000)
 
   return `${hours ? `${hours}h ` : ''}${minutes ? `${minutes}m ` : ''}${seconds}s`
+}
+
+export const formatStringToCreateDate = (date: Date, time: string): string => {
+  let month = date.getMonth().toString()
+  let day = date.getDate().toString()
+  if (date.getDate() < 10) {
+    day = '0' + date.getDate().toString()
+  }
+  if (date.getMonth() < 10) {
+    month = '0' + date.getMonth().toString()
+  }
+
+  return `${date.getFullYear().toString()}-${month}-${day}T${time}`
 }
--- front/src/layouts/DashboardLayout.vue
+++ front/src/layouts/DashboardLayout.vue
@@ -7,7 +7,74 @@
       <h1 class="flex-1 text-center">
         EpicHourly
       </h1>
-      <router-link :to="`/dashboard/profile/${storedUserID}`" class="flex-1 flex justify-end">
+
+      <div class="relative inline-block text-right w-80 py-1 px-1">
+        <button
+          type="button"
+          class="inline-flex items-center text-center px-1 py-1 border rounded-md shadow-sm text-sm font-medium bg-black focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+          @click="toggleDropdown"
+        >
+          <img alt="clockIn" src="../assets/clock-in.svg" class="w-8 h-8">
+          <svg
+            class="-mr-1 ml-2 h-5 w-5"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            fill="currentColor"
+            aria-hidden="true"
+          >
+            <path
+              fill-rule="evenodd"
+              d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+              clip-rule="evenodd"
+            />
+          </svg>
+        </button>
+        <div
+          v-if="isOpen"
+          class="origin-top-center absolute right-0 mt-2 w-full rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
+          role="menu"
+          aria-orientation="vertical"
+          aria-labelledby="options-menu"
+          @click="toggleDropdown"
+        >
+          <div class="py-1 text-center" role="none">
+            <p v-if="notificationList.length == 0" class="text-black">
+              You don't have notifications
+            </p>
+            <div v-for="notification in notificationList" :key="notification.message">
+              <a
+                v-if="notification.type == 'info'"
+                href="#"
+                class="rounded-xl px-4 py-2 text-sm text-gray-700 flex"
+                role="menuitem"
+              >
+                {{ notification.message }} : {{ notification.date.day }}/{{ notification.date.month }}/
+                {{ notification.date.year }}
+                <img
+                  alt="deleteCross"
+                  class="w-4 h-4 items-center"
+                  src="../assets/cross-small-svgrepo-com.svg"
+                  @click="deleteNotificationInList(notification)"
+                ></a>
+              <a
+                v-if="notification.type == 'warning'"
+                href="#"
+                class="rounded-xl px-4 py-2 text-sm text-gray-700 bg-yellow-500 flex"
+                role="menuitem"
+              >
+                {{ notification.message }} : {{ notification.date.day }}/{{ notification.date.month }}/
+                {{ notification.date.year }}
+                <img
+                  alt="deleteCross"
+                  class="w-4 h-4 items-center"
+                  src="../assets/cross-small-svgrepo-com.svg"
+                  @click="deleteNotificationInList(notification)"
+                ></a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <router-link :to="`/dashboard/profile/${storedUserID}`" class="flex justify-end">
         <img alt="profile" src="../assets/user_icon.svg" class="w-8 h-8">
       </router-link>
     </header>
@@ -77,9 +144,16 @@ import { useRouter } from 'vue-router'
 import { computed, onMounted, ref } from 'vue'
 import { apiUrl } from '../constants/urls'
 import { fetchData } from '../services/httpService'
+import { Notification } from '../models/Notification'
+import { createToast } from 'mosha-vue-toastify'
+import 'mosha-vue-toastify/dist/style.css'
+import { formatStringToCreateDate } from '../helpers/dateUtils'
 
 const router = useRouter()
 const storedUserID = ref('')
+const isOpen = ref<boolean>(false)
+const notificationList = ref<Notification[]>([])
+const userId = sessionStorage.getItem('userID')
 
 const logout = async () => {
   try {
@@ -94,6 +168,55 @@ const logout = async () => {
   }
 }
 
+const toggleDropdown = () => {
+  isOpen.value = !isOpen.value
+}
+
+const createNotificationInList = async () => {
+  const today = new Date()
+  var isLate = false
+  var isWorkLate = false
+  const dataUserSchedule = await fetchData(`${apiUrl}/api/schedules/${userId}`)
+  if (!dataUserSchedule) {
+    createToast(
+      { title: 'Please enter your working schedule' },
+      { transition: 'zoom', timeout: 8000, type: 'warning', position: 'bottom-right' }
+    )
+  }
+  const clockStatus = await fetchData(`${apiUrl}/api/clocks/${userId}/last`)
+  const clockDateStart = new Date(formatStringToCreateDate(today, dataUserSchedule.start_time))
+  if (clockDateStart < today && !clockStatus.clock.status) {
+    isLate = true
+  }
+  const clockDateEnd = new Date(formatStringToCreateDate(today, dataUserSchedule.end_time))
+  if (clockDateEnd > today && clockStatus.clock.status) {
+    isWorkLate = true
+  }
+  if (isLate) {
+    const newNotification = {
+      message: "You are late at your work, please clock'in",
+      type: 'warning',
+      date: { day: today.getDate(), month: today.getMonth(), year: today.getFullYear() }
+    }
+    notificationList.value.push(newNotification)
+  }
+
+  // vérifier si l'utilisateur fait des heures supplémentaires
+  if (isWorkLate) {
+    const newNotification = {
+      message: "You work hard today, don't forget to clock'out",
+      type: 'warning',
+      date: { day: today.getDate(), month: today.getMonth(), year: today.getFullYear() }
+    }
+    notificationList.value.push(newNotification)
+  }
+}
+
+const deleteNotificationInList = (notification: Notification) => {
+  const tempNotificationList = notificationList.value.filter((item) => item !== notification)
+  notificationList.value = tempNotificationList
+}
+
 const isManagerOrGeneralManager = computed(() => {
   const storedUserRole = sessionStorage.getItem('userRole')
   return storedUserRole === 'manager' || storedUserRole === 'general_manager'
@@ -105,7 +228,8 @@ const getUserInfo = () => {
   storedUserID.value = userID
 }
 
-onMounted(() => {
+onMounted(async () => {
   getUserInfo()
+  await createNotificationInList()
 })
 </script>
